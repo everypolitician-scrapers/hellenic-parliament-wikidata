e16fb6aa451e11f8818b076fa93143119fde9df9
Tony Bowden <tony@tmtm.com>
2016-03-02 09:01:17 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,9 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'colorize'
-require 'nokogiri'
-require 'pry'
-require 'scraperwiki'
 require 'wikidata/fetcher'
 
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
-
-def ids_from_claim(claim_str)
-  url = "https://wdq.wmflabs.org/api?q=claim[#{claim_str}]"
-  json = JSON.parse(open(url).read, symbolize_names: true)
-  json[:items].map { |id| "Q#{id}" }
+ids = EveryPolitician::Wikidata.wdq('claim[39:18915989]')
+ids.shuffle.each_slice(100) do |sliced|
+  EveryPolitician::Wikidata.scrape_wikidata(ids: ids, output: false)
 end
-
-ids_from_claim('39:18915989').each do |id|
-  data = WikiData::Fetcher.new(id: id).data or next
-  ScraperWiki.save_sqlite([:id], data)
-end
-
-require 'rest-client'
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
