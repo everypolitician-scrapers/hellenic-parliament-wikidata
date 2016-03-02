12c7e89812102347303e17d18b77292243ed56a3
Tony Bowden <tony@tmtm.com>
2016-03-02 09:12:10 +0000

    Grab in 50s

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,6 @@
 require 'wikidata/fetcher'
 
 ids = EveryPolitician::Wikidata.wdq('claim[39:18915989]')
-ids.shuffle.each_slice(100) do |sliced|
+ids.shuffle.each_slice(50) do |sliced|
   EveryPolitician::Wikidata.scrape_wikidata(ids: ids, output: false)
 end
