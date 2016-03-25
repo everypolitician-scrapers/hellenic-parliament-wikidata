8b6bb33b9ff8c763ef33028e61b273a6a1ca5a35
Tony Bowden <tony@tmtm.com>
2016-03-25 10:01:57 +0000

    fetch in batches of 100

--- scraper.rb
+++ scraper.rb
@@ -4,6 +4,4 @@
 require 'wikidata/fetcher'
 
 ids = EveryPolitician::Wikidata.wdq('claim[39:18915989]')
-ids.shuffle.each_slice(50) do |sliced|
-  EveryPolitician::Wikidata.scrape_wikidata(ids: ids, output: false)
-end
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids, batch_size: 100, output: false)
