d34206062b5e645311b816460a1c2f5a3a7806bc
Tony Bowden <tony@tmtm.com>
2016-06-21 09:21:53 +0100

    Find anyone with a Hellenic Parliament ID
    The P39 has been removed from lots of people, but many of them still
    have the P2278, so look up by that as well.

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,6 @@
 
 require 'wikidata/fetcher'
 
-ids = EveryPolitician::Wikidata.wdq('claim[39:18915989]')
-EveryPolitician::Wikidata.scrape_wikidata(ids: ids, batch_size: 100, output: false)
+member = EveryPolitician::Wikidata.wdq('claim[39:18915989]')
+withId = EveryPolitician::Wikidata.wdq('claim[2278]')
+EveryPolitician::Wikidata.scrape_wikidata(ids: member | withId, batch_size: 100, output: false)
