9b6fd0e8340a427c103012e79fe29560384b7ec2
Tony Bowden <tony@tmtm.com>
2016-07-29 21:24:20 +0100

    Switch to SPARQL query

--- scraper.rb
+++ scraper.rb
@@ -3,6 +3,20 @@
 
 require 'wikidata/fetcher'
 
-member = EveryPolitician::Wikidata.wdq('claim[39:18915989]')
-withId = EveryPolitician::Wikidata.wdq('claim[2278]')
+WIKIDATA_SPARQL_URL = 'https://query.wikidata.org/sparql'
+
+def wikidata_sparql(query)
+  result = RestClient.get WIKIDATA_SPARQL_URL, params: { query: query, format: 'json' }
+  json = JSON.parse(result, symbolize_names: true)
+  json[:results][:bindings].map { |res| res[:item][:value].split('/').last }
+rescue RestClient::Exception => e
+  abort "Wikidata query #{query.inspect} failed: #{e.message}"
+end
+
+# member = EveryPolitician::Wikidata.wdq('claim[39:18915989]')
+member = wikidata_sparql('SELECT ?item WHERE { ?item wdt:P39 wd:Q18915989 . }')
+
+# withId = EveryPolitician::Wikidata.wdq('claim[2278]')
+withId = wikidata_sparql('SELECT ?item WHERE { ?item wdt:P2278 ?dummy0 . }')
+
 EveryPolitician::Wikidata.scrape_wikidata(ids: member | withId, batch_size: 100, output: false)
