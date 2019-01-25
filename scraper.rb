268440024335cfa0fe4c0045873b5b816906f402
Tony Bowden <tony@tmtm.com>
2019-01-25 15:51:55 +0000

    Don't check EPdata
    Everyone matched in EP now has either a relevant P39 or a Hellenic
    Parliament ID, so we only need to check Wikidata itself.
    
    This should also cut down on memory use.

--- scraper.rb
+++ scraper.rb
@@ -1,15 +1,12 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'everypolitician'
 require 'wikidata/fetcher'
 
 # Position Held = Member of the Hellenic Parliament
-member = EveryPolitician::Wikidata.sparql('SELECT ?item WHERE { ?item wdt:P39 wd:Q18915989 . }')
+member = EveryPolitician::Wikidata.sparql('SELECT ?item WHERE { ?item wdt:P39 wd:Q18915989 }')
 
 # has Property: "Member of the Hellenic Parliament ID"
-withId = EveryPolitician::Wikidata.sparql('SELECT ?item WHERE { ?item wdt:P2278 ?dummy0 . }')
+withId = EveryPolitician::Wikidata.sparql('SELECT ?item WHERE { ?item wdt:P2278 [] }')
 
-existing = EveryPolitician::Index.new.country("Greece").lower_house.popolo.persons.map(&:wikidata).compact
-
-EveryPolitician::Wikidata.scrape_wikidata(ids: existing | member | withId, batch_size: 250)
+EveryPolitician::Wikidata.scrape_wikidata(ids: member | withId, batch_size: 250)
